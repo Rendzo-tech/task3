4f1290004fd06d3eaeb6217319b4420dcb138080
rendzo Maringa <qrendzo@gmail.com>
2024-08-06 14:49:16 +0200

    I have added comments to the file task_3.py.

--- task_3.py
+++ task_3.py
@@ -1,4 +1,6 @@
+# This section asks the user for their name and age
 input_name = input('Please enter your name: ')
 input_age = input('Please enter your age: ')
 
+# The user input is printed out along with the following statement
 print(f'Your name is {input_name} and you are {input_age} years old.')
